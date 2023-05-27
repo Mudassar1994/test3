af615386b89db6b4db53e755dc23dee80ec83ec5
Mudassar Khan <110166781+Mudassar1994@users.noreply.github.com>
2023-05-28 01:18:40 +0500

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 68){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:8000,
+        loop:true
+    });
+
+    $('.product .btn').click(function(){
+
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.product .box').show(400);
+        }else{
+            $('.product .box').not('.'+filter).hide(200);
+            $('.product .box').filter('.'+filter).show(400);
+        }
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
+
+    });
+
+});
